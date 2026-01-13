220ab8a4a2a5fb66a2eab87f2181dbbacc502c6f
faqihasyari <faqih8158@gmail.com>
2026-01-13 23:15:58 +0700

    add ui swagger

--- src/app.js
+++ src/app.js
@@ -11,6 +11,36 @@ app.get("/", (req, res) => {
   res.json({ status: "API is running 🚀" })
 })
 
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    customSiteTitle: "Todo API Documentation",
+    customCss: `
+      body {
+        background-color: #0f172a;
+      }
+      .swagger-ui {
+        color: #e5e7eb;
+      }
+      .topbar {
+        background-color: #020617;
+      }
+      .swagger-ui .opblock {
+        border-radius: 12px;
+        box-shadow: 0 10px 20px rgba(0,0,0,.4);
+      }
+      .swagger-ui .opblock-summary {
+        font-size: 16px;
+      }
+      .swagger-ui .btn.execute {
+        background-color: #22c55e;
+      }
+    `,
+  })
+)
+
+
 app.use("/todos", todoRoutes)
 
 export default app
