a5991bcf8163f66a26a08fc982fa18eac9815c0f
faqihasyari <faqih8158@gmail.com>
2026-01-14 13:30:09 +0700

    /

--- api/index.js
+++ api/index.js
@@ -1,15 +1,5 @@
-import { PrismaClient } from "@prisma/client"
+const { PrismaClient } = require("@prisma/client");
 
-const globalForPrisma = globalThis
+const prisma = new PrismaClient();
 
-const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"]
-  })
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
-}
-
-export default prisma
+module.exports = prisma;
